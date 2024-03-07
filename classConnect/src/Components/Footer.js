ed38e5c01dbf374246bb79a401c31d8b5a1fd35b
VinitM305 <114692166+VinitM305@users.noreply.github.com>
2024-03-08 01:41:04 +0530

    error in css file, updated

--- classConnect/src/Components/Footer.js
+++ classConnect/src/Components/Footer.js
@@ -4,22 +4,25 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="waves">
-        <div className="wave" id="wave1"></div>
-        <div className="wave" id="wave2"></div>
-        <div className="wave" id="wave3"></div>
-        <div className="wave" id="wave4"></div>
-      </div>
-      {/* <ul className="menu">
-        <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">About</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
-        <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
-      </ul> */}
-      <p>&copy;Copyright : ClassConnect</p>
-    </footer>
+    <div classname="foo">
+          <footer className="footer">
+          <div className="waves">
+            <div className="wave" id="wave1"></div>
+            <div className="wave" id="wave2"></div>
+            <div className="wave" id="wave3"></div>
+            <div className="wave" id="wave4"></div>
+          </div>
+          {/* <ul className="menu">
+            <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
+            <li className="menu__item"><a className="menu__link" href="#">About</a></li>
+            <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
+            <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
+            <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
+          </ul> */}
+          <p>&copy;Copyright : ClassConnect</p>
+        </footer>
+    </div>
+    
   );
 };
 
