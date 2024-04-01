502d53b078a6f142fa64052fe18b93bf05583624
VinitM305 <114692166+VinitM305@users.noreply.github.com>
2024-04-01 21:40:22 +0530

    footer updated

--- classConnect/src/Components/Footer.js
+++ classConnect/src/Components/Footer.js
@@ -6,12 +6,7 @@ const Footer = () => {
   return (
     <div className="foo">
           <footer className="footer">
-          <div className="waves">
-            <div className="wave" id="wave1"></div>
-            <div className="wave" id="wave2"></div>
-            <div className="wave" id="wave3"></div>
-            <div className="wave" id="wave4"></div>
-          </div>
+          
           <p>&copy;Copyright : ClassConnect by ProjectDAVS</p>
         </footer>
     </div>
