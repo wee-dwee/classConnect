dd9684a76dc26732d957218a5e69822f805bd5da
SankalpBohidar <sankalp.bohidar@gmail.com>
2024-04-10 21:55:25 +0530

    toggle button for unenroll

--- classConnect/src/Components/CreateCard.js
+++ classConnect/src/Components/CreateCard.js
@@ -4,11 +4,23 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import teacher from './teacher.png';
-
-const CreateCard = ({ classId, title, owner, classcode, isInstructor, profileId, setuserclassId,thisprofileId }) => {
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+const CreateCard = ({ classId, title, owner, classcode, isInstructor, profileId, setuserclassId, thisprofileId }) => {
   const [userProfile, setUserProfile] = useState(null);
   const [checkin, setCheckin] = useState(false);
-  
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   useEffect(() => {
     // Fetch user profile when component mounts
     fetchUserProfile(profileId);
@@ -35,6 +47,28 @@ const CreateCard = ({ classId, title, owner, classcode, isInstructor, profileId,
   return (
     <div className="joined__list">
       <div className="joined__wrapper">
+        <div className="joined__menu">
+          <IconButton aria-label="settings" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleClick} style={{ color: 'white' }}>
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Unenroll</MenuItem>
+          </Menu>
+        </div>
         <div className="joined__container">
           <div className="joined__imgWrapper" />
           <div className="joined__image" />
